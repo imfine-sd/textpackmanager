f7bed17e2757afc848de2a3b25f1eb5434459597
Cj-K-7 <kllt7059@gmail.com>
2023-08-17 23:59:01 +0900

    make plug-in-message handler

--- src/api/listener.ts
+++ src/api/listener.ts
@@ -1,12 +1,43 @@
-interface IframeData {
-  method: string;
+interface ImportData {
+  method: "import";
   param: string;
-  node?: SceneNode;
+}
+interface ExportData {
+  method: "export";
+  param: string;
+}
+interface SelectData {
+  method: "select";
+  param: SceneNode;
+}
+interface ChangeData {
+  method: "change";
+  param: SceneNode;
+}
+
+type PluginData = ImportData | ExportData | SelectData | ChangeData;
+
+function pluginDataHandler(data: PluginData) {
+  const { method, param } = data;
+
+  switch (method) {
+    case "import": {
+      //TODO : import xlsx or json
+    }
+    case "export": {
+      //TODO : export xlsx or json
+    }
+    case "change": {
+      //TODO : change node(param)
+    }
+    case "select": {
+    }
+  }
 }
 
 const onUImessage: MessageEventHandler = (pluginMessage): void => {
-  const data: IframeData = JSON.parse(pluginMessage);
-  console.log(data);
+  const pluginData: PluginData = JSON.parse(pluginMessage);
+  pluginDataHandler(pluginData);
 };
 
 export default onUImessage;
